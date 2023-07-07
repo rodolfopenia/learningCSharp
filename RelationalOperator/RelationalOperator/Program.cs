ee14673da4681e9208de7b5408c911e0559358ce
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-07 18:48:42 -0500

    Relational Operator Lesson

---
+++ RelationalOperator/RelationalOperator/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Runtime.InteropServices;
+
+var (valueOne, valueTwo, valueThree) = (10, 1, 2);
+
+Console.WriteLine("Los valores son: " + valueOne + valueTwo + valueThree);
+bool resultOne = valueOne == valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne == valueTwo: " + resultOne);
+
+bool resultTwo = valueOne != valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne != valueTwo: " + resultTwo);
+
+bool resultThree = valueOne > valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne > valueTwo: " + resultThree);
+
+bool resultFour = valueOne < valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne < valueTwo: " + resultFour);
+
+bool resultFive = valueOne >= valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne >= valueTwo: " + resultFive);
+
+bool resultSix = valueOne <= valueTwo;
+Console.WriteLine("El resultado de la comparación valueOne <= valueTwo: " + resultSix);
