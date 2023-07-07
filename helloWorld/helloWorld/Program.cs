940784d5ae154d50db69e7ea7cfcc06aa8129db9
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-07 12:48:18 -0500

    End of first exercise

--- helloWorld/helloWorld/Program.cs
+++ helloWorld/helloWorld/Program.cs
@@ -1,2 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Console.WriteLine("Hello, World!");
+
+// Programa que calcula el área de un rectángulo.
+double sideA;
+double sideB;
+double result;
+Console.WriteLine("Calcula el área de un rectángulo!!");
+Console.WriteLine("Ingrese el valor del lado A");
+sideA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese el valor del lado B");
+sideB = Convert.ToDouble(Console.ReadLine());
+result = sideA * sideB;
+Console.WriteLine("El resultado es: " + result);
+
+// Declarar var
+// Necesita inicializar
+var sideC = 15.2;
