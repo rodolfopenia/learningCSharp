05a8d61b9cfef7233849a6d6f0aad7bbc53847d3
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-07 18:17:57 -0500

    Logic Operator Lesson

---
+++ LogicOperators/LogicOperators/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+bool valueOne = true;
+bool valueTwo = false;
+bool valueThree = true;
+
+// Otra manera de declarar
+var (valueFour, valueFive, valueSix) = (true, true, false);
+
+// Si el valor 1 es verdadero y el valor 2 es verdadero, entonces el resultado es verdadero.
+// Todos los valores deben ser vedaderos para que el resultado sea verdadero
+bool resultAnd = valueOne && valueTwo;
+Console.WriteLine(resultAnd);
+
+// Solo basta que uno debe ser verdadero para que el resultado sea verdadero
+bool resultOr = valueOne || valueTwo || valueThree;
+Console.WriteLine(resultOr);
+
+bool resultAndOr = (valueOne && valueTwo) || valueThree;
+Console.WriteLine(resultAndOr);
+
+bool resultNot = !valueOne;
+Console.WriteLine(resultNot);
+
+// Validamos si los valores son diferentes entre si
+bool resultXor = valueOne ^ valueTwo;
+Console.WriteLine(resultXor);
