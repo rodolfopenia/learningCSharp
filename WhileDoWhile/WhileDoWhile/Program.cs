2b929c1c77021a82411795d37915ae6596b09147
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-07 22:15:46 -0500

    Loop For Lesson

--- WhileDoWhile/WhileDoWhile/Program.cs
+++ WhileDoWhile/WhileDoWhile/Program.cs
@@ -10,57 +10,66 @@
 int totalPlayer = 0;
 int totalBot = 0;
 int number = 0;
+int coins;
 string message = string.Empty;
 string switchControl = "menu";
 Random rnd = new Random();
 
 while(true)
 {
+    Console.WriteLine("Bienvenido al C a s i n o");
+    Console.WriteLine("¿Cuántas monedas quieres? (Necesitas una por juego): ");
+    //coins = Console.ReadLine();
+    int.TryParse(Console.ReadLine(), out coins);
     totalPlayer = 0;
     totalBot = 0;
-    switch (switchControl)
+
+    for(int i=0; i < coins; i++)
     {
-        case "menu":
-            Console.WriteLine("Bienvenido al C a s i n o");
-            Console.WriteLine("Escriba Black '21' para jugar al 21");
-            switchControl = Console.ReadLine();
-            break;
-        case "21":
-            do
-            {
-                number = rnd.Next(1, 13);
-                totalPlayer = totalPlayer + number;
-                Console.WriteLine("Toma tu primera carta, jugador. Te salió la carta: " + number);
-                Console.WriteLine("¿Deseas otra carta?: ");
-            } while (Console.ReadLine() == "Si");
+        switch (switchControl)
+        {
+            case "menu":
+                Console.WriteLine("Escriba Black '21' para jugar al 21");
+                switchControl = Console.ReadLine();
+                i--;
+                break;
+            case "21":
+                do
+                {
+                    number = rnd.Next(1, 13);
+                    totalPlayer = totalPlayer + number;
+                    Console.WriteLine("Toma tu primera carta, jugador. Te salió la carta: " + number);
+                    Console.WriteLine("¿Deseas otra carta?: ");
+                } while (Console.ReadLine() == "Si");
 
-            totalBot = rnd.Next(14, 23);
-            Console.WriteLine("El bot tiene: " + totalBot);
+                totalBot = rnd.Next(14, 23);
+                Console.WriteLine("El bot tiene: " + totalBot);
 
-            if (totalPlayer > totalBot && totalPlayer < 22)
-            {
-                message = "Venciste al bot, felicidades!";
-                switchControl = "menu";
-            }
-            else if (totalPlayer >= 22)
-            {
-                message = "Perdiste contra el bot, te pasaste del 21";
-                switchControl = "menu";
-            }
-            else if (totalPlayer <= totalBot)
-            {
-                message = "Perdiste contra el bot, lo sentimos";
-                switchControl = "menu";
-            }
-            else
-            {
-                message = "Condición no válida.";
-            }
-            Console.WriteLine(message);
-            break;
-        default:
-            Console.WriteLine("Valor ingresado no válido en el casino");
-            break;
+                if (totalPlayer > totalBot && totalPlayer < 22)
+                {
+                    message = "Venciste al bot, felicidades!";
+                    switchControl = "menu";
+                }
+                else if (totalPlayer >= 22)
+                {
+                    message = "Perdiste contra el bot, te pasaste del 21";
+                    switchControl = "menu";
+                }
+                else if (totalPlayer <= totalBot)
+                {
+                    message = "Perdiste contra el bot, lo sentimos";
+                    switchControl = "menu";
+                }
+                else
+                {
+                    message = "Condición no válida.";
+                }
+                Console.WriteLine(message);
+                break;
+            default:
+                Console.WriteLine("Valor ingresado no válido en el casino");
+                break;
+        }
     }
 }
 
