bfc39aefb047a757598c6bf85bc84a36d4cc5cee
Rodolfo Peña <rodolfo.penia@hotmail.com>
2023-07-07 19:21:10 -0500

    String Lesson

---
+++ Strings/Strings/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+
+using System.Collections.Specialized;
+
+Console.WriteLine("Rodolfo Peña ID");
+int height = 170;
+int years = 31;
+string name = "Rodolfo Peña";
+string documentIdentity = "44495551";
+var job = "System Engineer";
+
+string card = $"La información de {name} es la siguiente: \n" +
+    $"su edad es {years} años, su altura es {height}cm y su profesión es {job}";
+
+Console.WriteLine(card);
